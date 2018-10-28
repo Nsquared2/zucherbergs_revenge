2fddb229b0d6767409c96f2555a5ce2c7fc59558
mjm363 <mjm363@case.edu>
2018-10-28 18:28:38 -0400

    Added EasyAIPlayer test

--- src/test/java/AIHandlerTest.java
+++ src/test/java/AIHandlerTest.java
@@ -1,9 +1,16 @@
 import org.junit.Test;
+import org.junit.Assert;
 
 public class AIHandlerTest{
     @Test(expected = IllegalArgumentException.class)
     public void wrong_difficulty_exception(){
         AIPlayer ai = AIHandler.create_ai("not_a_difficulty", 1, "joe");
+   }
+
+   @Test
+    public void make_easy(){
+       AIPlayer ai = AIHandler.create_ai("easy", 1, "joe");
+       Assert.assertTrue(ai instanceof EasyAIPlayer);
    }
 
 }
