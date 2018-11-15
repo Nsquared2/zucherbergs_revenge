924f64da7e380194daa2e41b38ab5aad06120a3c
mjm363 <mjm363@case.edu>
2018-11-14 20:16:18 -0500

    Finished implementing AIHandler. This marks end of first draft of code which needs to be implemented for AI tests (excluding tests). Not debugged

--- src/main/java/AIHandler.java
+++ src/main/java/AIHandler.java
@@ -1,16 +1,16 @@
-//https://databricks.com/blog/2016/05/31/apache-spark-2-0-preview-machine-learning-model-persistence.html
-//https://stackoverflow.com/questions/33556543/how-to-save-model-and-apply-it-on-a-test-dataset-on-java
-//https://www.youtube.com/watch?v=wSB5oByt7ko
-
 import weka.classifiers.Classifier;
 import weka.classifiers.bayes.NaiveBayesUpdateable;
 import weka.classifiers.functions.SGD;
-import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.SelectedTag;
+import weka.core.converters.ArffLoader;
 import weka.core.converters.ConverterUtils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  * This class is responsible for maintaining the currently active AI players, creating new AI players,
@@ -38,7 +38,7 @@ public class AIHandler {
      * @param ids The list of ids of all game players
      * @return The new AI player, with the given ID and name, which will perform to the selected difficulty level
      */
-    public AIPlayer create_ai(String difficulty, int id, String name, ArrayList<Integer> ids){
+    public AIPlayer createAi(String difficulty, int id, String name, ArrayList<Integer> ids){
         AIPlayer ai_player;
 
         if(difficulty.equals("easy")){
@@ -68,29 +68,36 @@ public class AIHandler {
      * This method handles the training procedures for the medium and hard policies.
      * The easy difficulty does not require training, as the behavior is deterministic.
      */
-    void train_base_policies(){
-        //easy policy is deterministic so never trained
+    void trainBasePolicies(){
+        //easy policy is stochastic so never trained
 
         //medium policy training procedure
+        NaiveBayesUpdateable medium_policy_model = new NaiveBayesUpdateable();
+        trainClassifierWithGameData(medium_policy_model);
+        saveBaseModel(medium_model_path, medium_policy_model);
 
         //hard policy training procedure
+        SGD hard_policy_model = new SGD();
+        hard_policy_model.setLossFunction(new SelectedTag(SGD.LOGLOSS, SGD.TAGS_SELECTION));
+        trainClassifierWithGameData(hard_policy_model);
+        saveBaseModel(hard_model_path, hard_policy_model);
     };
+
 
     /**
      * Adds the round observations of all the AI players to the database for future base policy training
      * @param players Arraylist of all AI players that participated in the game
      */
-    void save_ai_player_observations(ArrayList<AIPlayer> players) {
+    void saveAiPlayerObservations(ArrayList<AIPlayer> players) {
         Instances dataset = WekaData.makeDataset();
         for (AIPlayer player : players) {
             //TODO: Append to ARFF file
-            dataset = dataset;
+            dataset = Instances.mergeInstances(dataset, player.round_instances);
         }
 
-        //TODO: load game id
-        int game_id = 0;
+        int save_id = getSaveGameId();
         try {
-            ConverterUtils.DataSink.write(data_file_name_prefix + Integer.toString(game_id), dataset);
+            ConverterUtils.DataSink.write(data_file_name_prefix + Integer.toString(save_id) + ".arff", dataset);
         }
         catch (Exception e){
             System.out.println("Exception: Game data not saved " + e.toString());
@@ -116,14 +123,65 @@ public class AIHandler {
         }
     }
 
+    private <C extends Classifier> void trainClassifierWithGameData(C model){
+        File data_dir = new File(data_path);
+        ArrayList<Integer> game_ids = new ArrayList<Integer>();
+
+        Instances dataset = WekaData.makeDataset();
+        for(File file: data_dir.listFiles()) {
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(file.getName()));
+                ArffLoader.ArffReader arff = new ArffLoader.ArffReader(reader);
+                Instances game_data = arff.getData();
+                game_data.setClassIndex(game_data.numAttributes() - 1);
+                //TODO: Avoid loading everything into RAM?
+                dataset = Instances.mergeInstances(dataset, game_data);
+            }
+            catch (Exception e){
+                System.out.println("Error: File " + file.getName() + " not used by training.");
+            }
+        }
+
+        System.out.println("Training base model...");
+        try {
+            model.buildClassifier(dataset);
+        }
+        catch (Exception e){
+            System.out.println("Exception in training base policies: " + e.toString());
+        }
+    }
+
     /**
-     * Takes two datasets and merges them
-     * @param dataset1 An Instances object
-     * @param dataset2 Another Instances object
-     * @return The merged instances object
+     * Saves a base policy to the model directory
+     * @param path the path to the save the model
+     * @param model the trained model object itself
+     * @param <C> Classifier type
      */
-    public Instances mergeInstances(Instances dataset1, Instances dataset2){
-        //TODO: Implement
-        return WekaData.makeDataset();
+    private <C extends Classifier> void saveBaseModel(String path, C model){
+        try {
+            weka.core.SerializationHelper.write(path, model);
+        }
+        catch (Exception e){
+            System.out.println("Error in saving model " + path + " due to " + e.toString());
+        }
+    }
+
+    /**
+     * Used to get id for game being currently saved
+     * @return (id of last saved game) + 1
+     */
+    private int getSaveGameId(){
+        File data_dir = new File(data_path);
+        ArrayList<Integer> game_ids = new ArrayList<Integer>();
+        for(File file: data_dir.listFiles()){
+            String name = file.getName();
+            try {
+                int id = Integer.valueOf(name.charAt(name.length() - 1));
+                game_ids.add(id);
+            }
+            catch (NumberFormatException e){}
+        }
+
+        return Collections.max(game_ids)+1;
     }
 }
