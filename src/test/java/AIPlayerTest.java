cb9955197b7d5828a5c04cc890b58206f38d400a
mjm363 <mjm363@case.edu>
2018-11-15 23:17:03 -0500

    More unit tests. Most of the test for AIHandler are now in place and working

--- src/test/java/AIPlayerTest.java
+++ src/test/java/AIPlayerTest.java
@@ -1,2 +1,25 @@
+import org.junit.Test;
+
 public class AIPlayerTest {
+    @Test
+    public void test_recieve_comms(){
+        AIPlayer ai = AIHandler.createAi("easy", 0, "joe", AITestUtil.dummy_ids(1));
+        Communication comm = new Communication(CommType.PROMISE_BETRAY, 0, 1);
+        ai.receiveMessage(0, comm);
+    }
+
+    @Test
+    public void round_action_test_easy(){
+        AIPlayer ai = AIHandler.createAi("easy", 0, "joe", AITestUtil.dummy_ids(1));
+        ai.round_action();
+
+    }
+
+    @Test
+    public void round_action_test_trainable(){
+        AITestUtil.setAIDirs();
+        AIPlayer ai = AIHandler.createAi("medium", 0, "joe", AITestUtil.dummy_ids(1));
+        ai.round_action();
+    }
+
 }
